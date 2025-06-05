af3b6bf1c311a2667f659eb80c5ee56554255ad9
Davidbuceoespana <davidbuceoespana@gmail.com>
2025-06-05 21:38:30 +0200

    Add files via upload

---
+++ components/oportunidades-venta.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import OportunidadesVenta from '../components/OportunidadesVenta';
+
+export default function PageOportunidadesVenta() {
+  return <OportunidadesVenta />;
+}
